db2bbdd65e71ce7df790caf3e75dc2b90137a37c
Andrzej Spiess <andrzej.spiess@gmail.com>
2018-07-06 22:21:46 +0200

    Uncommented pretty Show instance for Expr, added DS_Store files to gitignore

--- lib/Expression.hs
+++ lib/Expression.hs
@@ -18,11 +18,11 @@ where
 
 data Expr = Func String Expr
  | FuncApp Expr Expr
- | Name String deriving (Eq, Show)
+ | Name String deriving (Eq)
 
 -- Pretty Show Expr
 
---instance Show Expr where 
---  show (Func n e) = "𝞴" ++ show n ++ "." ++ show e
---  show (FuncApp e e1) = "(" ++ show e ++ " " ++ show e1 ++ ")"
---  show (Name s) = show s
+instance Show Expr where 
+  show (Func n e) = "𝞴" ++ show n ++ "." ++ show e
+  show (FuncApp e e1) = "(" ++ show e ++ " " ++ show e1 ++ ")"
+  show (Name s) = show s
